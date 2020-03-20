7d10a9ca3a98da224829f44a76e46f388713a5f1
IvAlekks <ivlelik2012@gmail.com>
2020-03-20 21:07:52 +0300

    max_shar.c

---
+++ max_dobl.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () 
+{
+double simb=' ';
+printf("simb:%c\n",simb);
+simb++;
+printf("simb:%c\n",simb);
+c= (int)simb
+printf("simb:%d\n",c);
+
+return 0;
---
+++ max_shar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () 
+{
+char simb=' ';
+printf("simb:%c\n",simb);
+simb++;
+printf("simb:%c\n",simb);
+int c;
+c= (int)simb;
+printf("simb:%d\n",c);
+
+return 0;
+}
